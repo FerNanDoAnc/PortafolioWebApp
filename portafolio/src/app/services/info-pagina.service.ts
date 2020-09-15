2fa7f7a9e3941df9bdca1c8f4de2859c714a2d30
Fera1999 <fernando.ancajima99@gmail.com>
2020-09-15 10:21:09 -0500

    interfaz para controlar info de la pagina agregada

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info:any={};
+  info:InfoPagina={};
   cargada=false;
 
   constructor(private http: HttpClient) {
-    
+
     //leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-        .subscribe(response =>{
+        .subscribe((response:InfoPagina) =>{
 
           this.cargada=true;
           this.info=response;
